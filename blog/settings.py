0dc48f98824751ebeae375b42870081faa98baa5
Fabiana <fabianarsantos@gmail.com>
2022-04-05 16:14:58 -0300

    s

--- blog/settings.py
+++ blog/settings.py
@@ -141,13 +141,3 @@ MESSAGE_TAGS = {
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-try:
-    from .local_settings import *
-except:
-    pass
